95a25cefb139fc6b0a10ef56e09dc703b67afeaf
Adura <babsaduragbemi021@gmail.com>
2021-11-08 04:55:44 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints 10 times the alphabet
+ *
+ *
+ * Return: Always (0)
+ */
+void print_alphabeta-x10(void){
+	int i, j;
+	while(i <= 10){
+		for(j = 'a'; j <= 'z'; j++){
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+	i++;
+}
