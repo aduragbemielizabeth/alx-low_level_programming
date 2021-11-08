f75b57688b41a329d7b6e17de41fa0f32b409f1e
Adura <babsaduragbemi021@gmail.com>
2021-11-08 07:14:47 -0800

    3-islower.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,13 +5,17 @@
  *
  * Return: Always (0)
  */
-void print_alphabeta-x10(void){
-	int i, j;
-	while(i <= 10){
-		for(j = 'a'; j <= 'z'; j++){
-			_putchar(j);
-		}
-		_putchar('\n');
-	}
-	i++;
+void print_alphabet_x10(void)
+{
+int n, co;
+co = 0;
+while (co < 10)
+{
+for (n = 'a'; n <= 'z'; n++)
+{
+_putchar(n);
+}
+co++;
+_putchar('\n');
+}
 }
